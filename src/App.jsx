ba4bf399c5fc0a0828cf3627dfe4d5e483361683
Raiff Said <raiff.said@gmail.com>
2022-08-16 22:23:33 -0300

    feature: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 
+import styles from './App.module.css';
 import './global.css';
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
     return (
         <>
             <Header />
 
-            <Post 
-                author="Raiff Said" 
-                content="Lorem ipsom dolor sit amet consectetur adipisicing elit."
-            />
-            <Post 
-                author="Diego Fernandes"
-                content="Um novo post"
-            />
+            <div className={styles.wrapper}>
+                <Sidebar />
+                
+                <main>
+                    <Post 
+                        author="Raiff Said" 
+                        content="Lorem ipsom dolor sit amet consectetur adipisicing elit."
+                    />
+                </main>
+            </div>
         </>
     );
 }
