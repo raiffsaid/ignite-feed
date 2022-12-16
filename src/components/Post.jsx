3377b8d3c749050ec3a398c92f940f4c897aac85
Raiff Said <raiff.said@gmail.com>
2022-12-16 19:36:55 -0300

    feat: Comment component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from "./Comment";
 import styles from "./Post.module.css"
 
 export function Post(params) {
@@ -37,6 +38,12 @@ export function Post(params) {
           <button type="submit">Publicar</button>
         </footer>
       </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   );
 }
